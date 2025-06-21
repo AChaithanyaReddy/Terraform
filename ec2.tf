8a08a39b9d8da9eadacdd29267f54d619cc6a017
Achaithanyareddy <chaithanya.reddy835@gmail.com>
2025-06-21 10:43:22 +0530

    become yes

---
+++ ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "allow_all_tf" {
+  name        = "allow_all_tf"
+  description = "Allow all inbound traffic and all outbound traffic to learn terraform"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  tags = {
+    Name = "allow_all_tf"
+  }
+}
+
+resource "aws_instance" "terraform" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all_tf.id]
+
+  tags = {
+    Name = "terraform"
+  }
+}
